367f3a81e7e2e31bc296365d96782df5439f032d
PeWilly <peter.f.w@outlook.com>
2019-03-24 14:07:11 +0100

    Hey hows it goinf

---
+++ index.html.py
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <link rel="stylesheet" type="text/css" href="style.css">
+   <title> Peters Page </title>
+ </head>
+ <body>
+<div id="header">
+ <div class="container">
+   <a id="header-title" href="index.html">PETER'S PAGE</a>
+    <ul id="header-nav">
+     <li><a href="index0.html">About</a></li>
+     <li><a href="mailto: Peter.f.w@outlook.com">Contact</a></li>
+    </ul>
+ </div>
+</div>
+ <div id="content">
+    <div class="container">
+     <div class="post">
+       <div class="post-author">
+         <img src="acorn_PNG37019.png">
+              <span>Jonathan Franks</span>
+       </div>
+ <p class="post-date">3.3.2019</p>
+ <h3 class="post-title">Becoming a web developer</h3>
+   <div class="post-content">
+     <div class="box" id="box1">
+   <p>If somebody would have told me a week a go that I would be able to make a website, I would not have believed them.</p>
+   <p><img src="https://getmimo.com/r/profile.svg">Just a few days later, I came across Mimo and started to work on a real blog.Just a few days later, I came across Mimo and started to work on a real blogJust a few days later, I came across Mimo and started to work on a real blogJust a few days later, I came across Mimo and started to work on a real blog.Just a few days later, I came across Mimo and started to work on a real blogJust a few days later, I came across Mimo and started to work on a real blogJust a few days later, I came across Mimo and started to work on a real blogJust a few days later, I came across Mimo and started to work on a real blog.
+   </p>
+     </div>
+    </div>
+   </div>
+  </div>
+  <div class="container">
+    <div class="post">
+      <div class="post-author">
+      <img src="acorn_PNG37019.png">
+       <span>Alfons Hetler</span>
+ </div>
+ <p class="post-date">19.2.2019</p>
+ <h3 class="post-title">What I know that you want to know</h3>
+   <div class="post-content">
+     <div class="box" id="box1">
+    <p>If somebody would have told me a week a go that I would be able to make a website, I would not have believed them.</p>
+    <p><img src="https://getmimo.com/r/profile.svg">Just a few days later, I came across Mimo and started to work on a real blog.Just a few days later, I came across Mimo and started to work on a real blogJust a few days later, I came across Mimo and started to work on a real blogJust a few days later, I came across Mimo and started to work on a real blog.Just a few days later, I came across Mimo and started to work on a real blogJust a few days later, I came across Mimo and started to work on a real blogJust a few days later, I came across Mimo and started to work on a real blogJust a few days later, I came across Mimo and started to work on a real blog.
+  </p>
+   </div>
+  </div>
+ </div>
+</div>
+<div id="footer">
+ <div class="container">
+
+  <div class="column">
+  <h4>My Links</h4>
+  <p>
+   <a href="#">https://vsco.co/williamspeter/images/1</a><br>
+   <a href="#">https://peterfw9.wixsite.com/pfwphotography</a>
+  </p>
+ </div>
+
+ <div class="column">
+   <h4>My Story</h4>
+   <p>Hi there! I'm an aspiring web developer.</p>
+     </div>
+    </div>
+   </div>
+  </div>
+ </body>
+</html>
---
+++ index2.html.py
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+ <head>
+   <link rel="stylesheet" type="text/css" href="style.css">
+   <title> Peters Page </title>
+ </head>
+ <body>
+ <div id=header>
+   <div class="container"
+   <a href="index.html">Peter's Page</a>
+   <ul>
+     <li><a href="about.html">About</a></li>
+     <li><a href="mailto: Peter.f.w@outlook.com">Contact</a></li>
+   </ul>
+   </div>
+ </div>
+ <div id=content>
+   <div class="container"
+   <div class="about">
+
+ <h1 class="about-title">
+   Becoming a web developer</h1>
+   <div class="about-content">
+ <p>If somebody would have told me a week a go that I would be able to make a website, I would not have believed them.
+ </p>
+ <p>Just a few days later, I came across Mimo and started to work on a real blog.
+ </p>
+  </div>
+   </div>
+ </div>
+ <div id=footer>
+   <div class="container"
+   <div class="column">
+     <h4>My Links</h4>
+     <p>
+       <a href="https://vsco.co/williamspeter/images/1">VSCO</a>
+       <br>
+       <a href="https://peterfw9.wixsite.com/pfwphotography">WIXPage</a>
+     </p>
+   </div>
+   <div class="column">
+     <h4>My Story</h4>
+     <p>Hi je m'apelle Peter et je veux comprends la langue d'un'ordinateur.</p>
+
+   </div>
+  </div>
+ </div>
+ </body>
+</html>
